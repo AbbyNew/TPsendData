d78beeac14a6d725c94120d3e6725f67e7f15820
AbbyNew <x.nouvelle@live.fr>
2021-04-06 16:35:49 +0200

    Ajout d'un message temporaire (setTimeout) quand un formulaire vide est envoyé

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // on récupère l'élément form  
 const theForm = document.getElementById("form") ; 
+// on récupère la valeur de l'input
+const trucAEnvoyer = document.getElementById("value") ; 
+// on récupère l'élément result
+const res = document.getElementById("result") ; 
+
 
 console.log(theForm) ;
 
@@ -8,15 +13,16 @@ theForm.addEventListener("submit", function(event) {
     
     event.preventDefault() ; // pour éviter que la page se recharge (et donc que le contenu de la forme disparaisse) 
     console.log("dans le submit") ;
-
-    // on récupère la valeur de l'input
-    const trucAEnvoyer = document.getElementById("value") ; 
-
     console.log(trucAEnvoyer.value) ;
 
     if (trucAEnvoyer.value.length == 0) {
-        alert("You must enter something") ; // pas d'input-> alerte, input est vide
-        res.innerText = "" ;
+
+        setTimeout(function() {
+            res.innerText = "" ; 
+        }, 3000);        
+        
+        //alert("You must enter something !") ; // pas d'input-> alerte, input est vide
+        res.innerText = "You must enter something !" ;
     }
     else
         sendrequest(trucAEnvoyer) ; // input-> on l'envoie
@@ -44,8 +50,7 @@ function sendrequest(leTrucAEnvoyer) {
             
             // pour avoir une partie de l'objet: 
             var response = JSON.parse(this.responseText) ;        
-            var responseShort = response.postData.text;     
-            const res = document.getElementById("result") ; // on récupère l'élément result  
+            var responseShort = response.postData.text;              
             //res.innerText = JSON.stringify(responseShort); // on lui associe la réponse
             res.innerText = responseShort; // on lui associe la réponse
         }
